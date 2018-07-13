782119aa1b3e874177b22a5ccd3dee21e3e11383
sanahsuri <sanah@github.com>
2018-07-13 15:38:17 -0500

    inital commits

--- conic.java
+++ conic.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
-public class Part4 {
-	
+public class conic {
+
 	// function to get values
 	public static ArrayList<Double> getPoints() {
 		ArrayList<Double> points = new ArrayList<Double>();
-		Scanner sc = new Scanner(System.in); 
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a, b, c values:");
 		for (int i = 0; i < 3; i++) {
 			points.add(sc.nextDouble());
@@ -16,19 +16,19 @@ public class Part4 {
 		System.out.println();
 		return points;
 	}
-	
-	// function that returns updated s value 
+
+	// function that returns updated s value
 	public static double sVal(double initialS, double digits) {
-		double s = initialS; 
+		double s = initialS;
 		double k = digits - 1;
-		int i = 0; 
+		int i = 0;
 		while (i < k) {
-			s = ((s * 10) + initialS);  
-			i++; 
+			s = ((s * 10) + initialS);
+			i++;
 		}
-		return s; 
+		return s;
 	}
-	
+
 	// function that returns points with given number of digits
 	public static ArrayList<Double> updatePoints(ArrayList<Double> points, double digits, double initialS) {
 		ArrayList<Double> lst = new ArrayList<Double>(6);
@@ -40,17 +40,17 @@ public class Part4 {
 		for (int n = 0; n < 3; n++) {
 			lst.add(initialS - points.get(n));
 		}
-		
+
 		int i = 0;
-		int h = 0; 
+		int h = 0;
 		double k = digits - 1;
-		if (k != 0) { 
+		if (k != 0) {
 		while (i < k) {
 			ret.set(0, ret.get(0) * 10 + lst.get((1 + h) % 6));
 			ret.set(1, ret.get(1) * 10 + lst.get((2 + h) % 6));
 			ret.set(2, ret.get(2) * 10 + lst.get((3 + h) % 6));
 			i++;
-			h++; 
+			h++;
 		}
 		double s = sVal(initialS, digits);
 		for (int m = 0; m < 3; m++) {
@@ -61,25 +61,25 @@ public class Part4 {
 			return lst;
 		}
 	}
-	
-	// function that determines type of conic 
+
+	// function that determines type of conic
 	public static void conicType(ArrayList<Double> points, double initialS, double digits) {
-		
-		double a = points.get(0); 
+
+		double a = points.get(0);
 		double b = points.get(1);
 		double c = points.get(2);
-		double s = sVal(initialS, digits); 
+		double s = sVal(initialS, digits);
 		double p1 = ((a*a)-(a*b)+(a*c)-(a*s)-(b*b)+(b*c)+(b*s)-(c*c));
 		double p2 = ((a*a)-(a*b)-(a*c)+(b*b)+(b*c)-(b*s)-(c*c)+(c*s));
 		double p3 = ((a*a)+(a*b)-(a*c)-(a*s)+(b*b)+(b*c)-(2*b*s)+(c*c)-(c*s)+(s*s));
 		double p4 = ((a*a)+(a*b)+(a*c)-(2*a*s)-(b*b)+(b*c)+(c*c)-(2*c*s)+(s*s));
-		
+
 		System.out.println("P1: " + p1);
 		System.out.println("P2: " + p2);
 		System.out.println("P3: " + p3);
 		System.out.println("P4: " + p4);
-		
-		if ((p1 > 0 && p2 > 0 && p3 > 0 && p4 > 0) || 
+
+		if ((p1 > 0 && p2 > 0 && p3 > 0 && p4 > 0) ||
 				(p1 < 0 && p2 < 0 && p3 > 0 && p4 > 0) ||
 				(p1 < 0 && p2 > 0 && p3 < 0 && p4 > 0) ||
 				(p1 < 0 && p2 > 0 && p3 > 0 && p4 < 0) ||
@@ -105,17 +105,17 @@ public class Part4 {
 		System.out.println();
 
 	}
-	
-	// main driver  
+
+	// main driver
 	public static void main(String args[]) {
-		ArrayList<Double> points = getPoints(); 
+		ArrayList<Double> points = getPoints();
 		ArrayList<Double> lst = new ArrayList<Double>();
 		double s = points.remove(3);
 		for (int i = 1; i <= 10; i++) {
 			lst = updatePoints(points, i, s);
-			conicType(lst, s, i); 
+			conicType(lst, s, i);
 		}
-		
+
 	}
 
 }
